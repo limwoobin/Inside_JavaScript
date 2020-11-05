fc4fe9cbb57cb7f5c0940dc8614f8a15254fa7a3
limwoobin <drogba02@naver.com>
2020-11-05 18:22:12 +0900

    Java Script 기본 타입

---
+++ Chapter03/Ex_1.js
@@ -0,0 +1,21 @@
+var intNum = 10;
+var floatNum = 0.1;
+
+var singleQuoteStr = 'zz';
+var doubleQuoteStr = "zzz";
+var singleChar = 'a';
+
+var booleanVar = true;
+
+var emptyVar;
+
+var nullBar = null;
+
+console.log(typeof intNum 
+          , typeof floatNum
+          , typeof singleQuoteStr
+          , typeof doubleQuoteStr
+          , typeof singleChar
+          , typeof booleanVar
+          , typeof emptyVar
+          , typeof nullBar);
